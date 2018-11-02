0d660e583de90cdfc0d3d42f116a18fc663f8d5e
Faraj Daoud <faraj.daoud@gmail.com>
2018-11-02 14:08:13 -0400

    Added HTML

--- src/index.js
+++ src/index.js
@@ -13,6 +13,27 @@ class App extends React.Component {
     render() {
         return(
             <div id="app-wrapper">
+                <div id="break-container">
+                    <span id="break-label">Break Length</span>
+                    <span id="break-length"></span>
+                    <button id="break-decrement">decrement</button>
+                    <button id="break-increment">increment</button>
+                </div>
+                <div id="session-container">
+                    <span id="session-label">Session Length</span>
+                    <span id="session-length"></span>
+                    <button id="session-decrement">decrement</button>
+                    <button id="session-increment">increment</button>
+                </div>
+                <div id="timer-container">
+                    <span id="timer-label">Session</span>
+                    <span id="time-left"></span>
+                    <div id="start_stop">
+                        <button id="toggle_start_stop">Start</button>
+                    </div>
+                    <button id="reset">Reset</button>
+                </div>
+               
                 
             </div>
         );
